a663fe92c261c5c4405d48c82de883e2f8e83999
Oliver-Merkel <162693101+Oliver-Merkel@users.noreply.github.com>
2024-05-04 15:50:03 +0200

    Update adder.py

--- adder.py
+++ adder.py
@@ -5,16 +5,19 @@ some adding function
 
 >>> add(2,3)
 5
-
->>> add(-5,-3)
--8
-
->>> add(2,0)
-2
 """
 
 def add(x, y):
-    """ add two numbers """
+    """ add two numbers
+    >>> add(2,3)
+    5
+    
+    >>> add(-5,-3)
+    -8
+    
+    >>> add(2,0)
+    2
+    """
     return x+y
 
 if __name__ == "__main__":
