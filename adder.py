92c32c83c9deb52b13307507e513d61861834bba
Oliver-Merkel <162693101+Oliver-Merkel@users.noreply.github.com>
2024-05-04 15:31:22 +0200

    Create adder.py

---
+++ adder.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+"""
+some adding function 
+
+>>> add(2,3)
+5
+
+>>> add(-5,-3)
+-8
+
+>>> add(2,0)
+2
+"""
+
+def add(x, y):
+    """ add two numbers """
+    return x+y
+
+if __name__ == "__main__":
+    print(add(2,3))
